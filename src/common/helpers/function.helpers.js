6d12d563f395b35f26b53afa3d26d0fe32d6f6c7
Leonardo <luisfonseca9521@gmail.com>
2022-10-16 13:11:17 -0500

    add general functions helpers

---
+++ src/common/helpers/function.helpers.js
@@ -0,0 +1,22 @@
+exports.generateRandomPassword = () => {
+  const chars =
+    'abcdefghijklmnopqrstubwsyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890|!.$%&';
+  let password = '';
+
+  for (i = 0; i < 11; i++) {
+    password += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
+  return password;
+};
+
+exports.validateEmail = (inputText) => {
+  const regex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return inputText.match(regex) ? true : false;
+};
+
+exports.validatePhoneNumber = (phone) => {
+  const phoneRegexp = /^[0-9]{10}$/;
+  return phoneRegexp.test(phone);
+};
