3b8020858564b5b7b08efb7a3dc93faed0b827b6
Leonardo <luisfonseca9521@gmail.com>
2022-10-30 18:24:05 -0600

    add nodemailer services

---
+++ src/common/services/nodemailer.services.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+
+class NodemailerServices {
+  constructor() {}
+
+  async sendEmail(payload) {
+    try {
+      let account = await nodemailer.createTestAccount();
+
+      let transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        auth: {
+          user: account.user,
+          pass: account.pass,
+        },
+      });
+
+      await transporter.sendMail({
+        from: 'development@devs.mx', // sender address
+        to: payload.email, // list of receivers
+        subject: payload.subject, // Subject line
+        text: payload.text, // plain text body
+        // html: '<b>Hello world?</b>', // html body
+      });
+
+      return;
+    } catch (error) {}
+  }
+}
+
+module.exports = new NodemailerServices();
