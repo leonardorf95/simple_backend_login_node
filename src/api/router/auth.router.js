0309dd1dfe7702791d50fd42ecd6d22100396592
Leonardo <35271048+leonardorf95@users.noreply.github.com>
2025-06-04 14:20:58 -0600

    Update auth.router.js

--- src/api/router/auth.router.js
+++ src/api/router/auth.router.js
@@ -1,13 +1,7 @@
 const router = require('express-promise-router')();
 const authController = require('../controllers/auth.controller');
-const passport = require('../session');
 
-router
-  .route('/login')
-  .post([
-    passport.authenticate('local', { session: false }),
-    authController.login,
-  ]);
+router.route('/login').post(authController.login);
 
 router.route('/logout').get(authController.logout);
 
