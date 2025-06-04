c4d4352fe677c2cadf3d11c1b89610835e641f98
Leonardo <35271048+leonardorf95@users.noreply.github.com>
2025-06-04 14:21:44 -0600

    Update auth.controller.js

--- src/api/controllers/auth.controller.js
+++ src/api/controllers/auth.controller.js
@@ -1,13 +1,32 @@
 const handlerResponse = require('../../common/handlers/response.handler');
 const codes = require('../../common/helpers/errorCodes.helpers');
 const sessionService = require('../services/sessions.services');
+const passport = require('../session');
 
 class AuthController {
   constructor() {}
 
-  async login(req, res) {
-    return res.status(200).send(req.user);
+  async login(req, res, next) {
+    passport.authenticate('local', { session: false }, (err, user, info) => {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error en el servidor',
+          description: err.message || 'Error inesperado',
+        });
+      }
+
+      if (!user) {
+        // Cuando la estrategia retorna done(null, false, { message: ... })
+        return res.status(401).json({
+          message: info?.message || 'Credenciales inválidas',
+        });
+      }
+
+      // Autenticación exitosa
+      return res.status(200).send(user); // `user` contiene tu sessionData
+    })(req, res, next);
   }
+
 
   async logout(req, res) {
     const userId = req.user.id;
